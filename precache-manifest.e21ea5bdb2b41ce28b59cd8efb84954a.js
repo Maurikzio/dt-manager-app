ff5cbb904fc6c5fb62de1e571e9a83bbb1ffecab
Maurikzio <m.morocho1196@gmail.com>
2020-04-26 19:26:53 +0300

    Updates

---
+++ precache-manifest.e21ea5bdb2b41ce28b59cd8efb84954a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356cd1b90d2ba6e69b4f73d028dd33e1",
+    "url": "/dt-manager-app/index.html"
+  },
+  {
+    "revision": "6c7d4e7aa73917b91598",
+    "url": "/dt-manager-app/static/css/main.73290347.chunk.css"
+  },
+  {
+    "revision": "6f499bc7dec512fe3192",
+    "url": "/dt-manager-app/static/js/2.ec4c31da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dt-manager-app/static/js/2.ec4c31da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7d4e7aa73917b91598",
+    "url": "/dt-manager-app/static/js/main.ab5f50fd.chunk.js"
+  },
+  {
+    "revision": "50d26fddf0d4c90a51a5",
+    "url": "/dt-manager-app/static/js/runtime-main.fbaf9800.js"
+  },
+  {
+    "revision": "fc727f8733a53898735a70cf6b492fe8",
+    "url": "/dt-manager-app/static/media/luke-skywalker.fc727f87.png"
+  }
+]);
